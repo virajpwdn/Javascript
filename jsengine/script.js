b397be717a50b86b010e58c9b762f22b57b32eee
virajpwdn <virajpatwardhan6@gmail.com>
2025-04-05 01:17:48 +0530

    binary trees

--- jsengine/script.js
+++ jsengine/script.js
@@ -1,9 +1,24 @@
-var b = 100;
-{
-    let a = 10;
-    var b = 20;
-    const c = 30;
+// var b = 100;
+// {
+//     let a = 10;
+//     var b = 20;
+//     const c = 30;
 
+// }
+
+// console.log(b);
+
+let arr = [1,2,3,4,5];
+let obj = {
+    username: "macbook",
+    brand: "Apple"
 }
 
-console.log(b);
+
+function abc(a,b){
+    console.log(this);
+    console.log(a);
+    console.log(b);
+}
+
+abc.apply(obj, [10, 20]);
